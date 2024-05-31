58e4ab7e8534a6e44bb23634226dc6f8ca8d47d8
Anastasiia Vdovina <vdovina.nastya03@gmail.com>
2024-05-31 20:12:54 +0300

    Player

--- Player.cpp
+++ Player.cpp
@@ -5,7 +5,7 @@
 #include <string>
 
 Player::Player(std::string name, Pet* pet, int playerSteps): name_(name), pet_(pet), playerSteps_(playerSteps){}
-Player::Player(): name_("\0"), pet_(nullptr), playerSteps_(0){}
+Player::Player(): name_(""), pet_(nullptr), playerSteps_(0){}
 
 void Player::increasePlayerSteps(){
     ++playerSteps_;
